10fbf15ab5f9fdb9a967ba485c5009fce53d168d
Samarth2001 <samarthsr2001@gmail.com>
2023-12-05 02:06:24 +0530

    first commit

---
+++ hi/src/pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import styles from '../styles/Home.module.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello World!</h1>
+    </div>
+  );
+};
+
+export default Home;
