9cc537ddfbbd874ded55802bb49c90b16096edd4
Samarth Rayar <59824124+Samarth2001@users.noreply.github.com>
2023-12-20 17:49:09 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Hello World!!!!</h1>
+      <h1 className={styles.title}>Redeploying Soon!!!!</h1>
     </div>
   );
 };
